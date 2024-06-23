db1ca4756aa992e48d83169850389bcd80ecc7bc
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-23 21:21:11 +0900

    bj_ë입출력_나머지

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -1,9 +1,23 @@
 package com.ki;
-
+//예제 입력 1 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C
+//        5 8 4
+//예제 출력 1
+//        1
+//        1
+//        0
+//        0
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
 
-
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
     }
 }
