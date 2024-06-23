177d76dd2c3cc3aa15435efda59fe83ed60bc0d5
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-06-23 22:55:48 +0900

    백준 별찍기(2)

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -9,12 +9,14 @@ public class Main {
         int num = sc.nextInt();
 
         for (int i = 1; i <= num; i++) {
+            for(int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
             for (int j = 1; j <= i; j++) {
-                System.out.printf("*");
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
 
