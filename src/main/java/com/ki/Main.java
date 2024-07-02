6d3eed67608634b6d035a45067b2973de83469a4
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-02 20:41:33 +0900

    백준 EOF(End of File) -> ãhasNext(); 활용

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -7,16 +7,15 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        int sum = num;
+        int a, b, sum;
 
-        for (int i = 1; i <= num; i++) {
-            System.out.print(" ".repeat(sum - 1));
-            sum -= 1;
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        while (sc.hasNextInt()) {
+            a = sc.nextInt();
+            b = sc.nextInt();
+            sum = a + b;
+            System.out.println(sum);
         }
+        sc.close();
     }
 }
+
