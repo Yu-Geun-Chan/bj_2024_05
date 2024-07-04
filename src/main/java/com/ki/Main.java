faaa76f83b22d02c326cdf4bfa12c2e9ad5c5f7a
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-04 21:40:35 +0900

    백준 공넣기

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -7,21 +7,22 @@ class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        int[] numbers =  new int[num];
 
-        for (int i = 0; i < num; i++) {
-            int a = sc.nextInt();
-            numbers[i] = a;
-        }
+        int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        int j = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (num2 == numbers[i]) {
-                j++;
+        int[] arr = new int[num1];
+
+        for (int i = 1; i <= num2; i++) {
+            int num3 = sc.nextInt();
+            int num4 = sc.nextInt();
+            int num5 = sc.nextInt();
+            for (int j = num3; j <= num4; j++) {
+                arr[j - 1] = num5;
             }
         }
-        System.out.println(j);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
 
