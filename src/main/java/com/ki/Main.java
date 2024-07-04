4418e013bfcb3aba57f6723d2508d5c421862c51
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-07-04 23:36:25 +0900

    백준 과제안낸사람

--- src/main/java/com/ki/Main.java
+++ src/main/java/com/ki/Main.java
@@ -7,22 +7,19 @@ class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[30];
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-        int[] arr = new int[num1];
-
-        for (int i = 1; i <= num2; i++) {
-            int num3 = sc.nextInt();
-            int num4 = sc.nextInt();
-            int num5 = sc.nextInt();
-            for (int j = num3; j <= num4; j++) {
-                arr[j - 1] = num5;
-            }
+        for (int i = 0; i < 28; i++) {
+            int num = sc.nextInt();
+            arr[num - 1] = 1;
         }
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+
+        for (int i = 0; i < 30; i++) {
+            if (arr[i] != 1) {
+                System.out.println(i + 1);
+            }
         }
     }
 }
+
 
